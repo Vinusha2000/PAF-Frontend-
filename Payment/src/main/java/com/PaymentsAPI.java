d0af2d1fe89be1efff6493b2f905c4ceec1ad012
M.A.Vinusha Lankani Muthukudaarachchi <71536924+Vinusha2000@users.noreply.github.com>
2022-05-15 22:10:07 +0530

    IT20139308
    Payment Management

---
+++ Payment/src/main/java/com/PaymentsAPI.java
@@ -0,0 +1,100 @@
+package com;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PaymentsAPI
+ */
+@WebServlet("/PaymentsAPI")
+public class PaymentsAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Payment paymentObj = new Payment();
+    /**
+     * Default constructor. 
+     */
+    public PaymentsAPI() {
+    	super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String output = paymentObj.insertPayment(request.getParameter("AccNumber"), 
+				   request.getParameter("DateOfPayment"), 
+				   request.getParameter("Amount"), 
+				   request.getParameter("TypeOfPayment")); 
+		
+                     response.getWriter().write(output);
+		
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Map<String, String> paras = getParasMap(request); 
+		 String output = paymentObj.updatePayment(paras.get("hidPaymentIDSave").toString(), 
+				 							paras.get("AccNumber").toString(), 
+				 							paras.get("DateOfPayment").toString(), 
+				 							paras.get("Amount").toString(), 
+				 							paras.get("TypeOfPayment").toString()); 
+		response.getWriter().write(output); 
+	
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Map<String, String> paras = getParasMap(request); 
+		 String output = paymentObj.deletePayment(paras.get("PaymentID").toString()); 
+		response.getWriter().write(output);
+	}
+	
+	private static Map<String, String> getParasMap(HttpServletRequest request) 
+	{ 
+	 Map<String, String> map = new HashMap<String, String>(); 
+	try
+	 { 
+	 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+	 String queryString = scanner.hasNext() ? 
+	 scanner.useDelimiter("\\A").next() : ""; 
+	 scanner.close(); 
+	 String[] params = queryString.split("&"); 
+	 for (String param : params) 
+	 {
+		 String[] p = param.split("=");
+		 map.put(p[0], p[1]); 
+		 } 
+		 } 
+		catch (Exception e) 
+		 { 
+		 } 
+		return map; 
+		}
+
+}
